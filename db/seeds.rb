86e104262bc7691491306630e7a82baa573aa75a
Hasan <hasanadeym@gmail.com>
2021-11-23 15:41:00 +0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# frozen_string_literal: true
+
+default_queue = JobQueue.find_or_create_by!(name: 'default')
+default_job = default_queue.background_jobs.find_by(name: 'default_background_job') || default_queue.background_jobs.create!(
+  name: 'default_background_job',
+  priority: :high,
+  status: :waiting,
+  starts_at: DateTime.now + 1.day,
+  expires_at: DateTime.now + 2.days
+)
